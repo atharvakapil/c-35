df3e9f74f613292cac8d9e93ad1c5a97f6cb3d4b
atharvakapil <77534010+atharvakapil@users.noreply.github.com>
2021-09-11 15:46:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,111 @@
+var balloon,balloonImage1,balloonImage2;
+var database;
+var height;
+
+function preload(){
+   bg =loadImage("Images/cityImage.png");
+   balloonImage1=loadAnimation("Images/HotAirBallon01.png");
+   balloonImage2=loadAnimation("Images/HotAirBallon01.png","Images/HotAirBallon01.png",
+   "Images/HotAirBallon01.png","Images/HotAirBallon02.png","Images/HotAirBallon02.png",
+   "Images/HotAirBallon02.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png");
+  }
+
+//Function to set initial environment
+function setup() {
+
+   database=firebase.database();
+
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,650,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  var balloonHeight=database.ref('balloon/height');
+  balloonHeight.on("value",readHeight, showError);
+
+
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    updateHeight(-10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale+0.005;
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+ function updateHeight(x,y){
+   database.ref('balloon/height').set({
+     'x': height.x + x ,
+     'y': height.y + y
+   })
+ }
+
+
+//CHOOSE THE CORRECT READHEIGHT FUNCTION
+// function readHeight(data){
+//   balloon.x = height.x;
+//   balloon.y = height.y;
+// }
+
+// function readHeight(data){
+//   height = data.val();
+//   balloon.x = height.x;
+//   balloon.y = height.y;
+// }
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJACBtZf3HA8FG-L7x_iPte63yQJUETGA",
+  authDomain: "c-35-c4a4c.firebaseapp.com",
+  databaseURL: "https://c-35-c4a4c-default-rtdb.firebaseio.com",
+  projectId: "c-35-c4a4c",
+  storageBucket: "c-35-c4a4c.appspot.com",
+  messagingSenderId: "118812357706",
+  appId: "1:118812357706:web:28a875001606ffca16a2b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// function readHeight(data){
+//   height = data.val();
+// }
+
+// function readHeight(){
+//   height = val();
+//   balloon.x = height.x;
+//   balloon.y = height.y;
+// }
+
+function showError(){
+  console.log("Error in writing to the database");
+}
